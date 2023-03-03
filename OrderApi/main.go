eb2afaf826fc1e02378dc25824a0a98d565228d9
likyam <384945266@qq.com>
2023-03-03 17:42:39 +0800

    配置链路追踪

--- OrderApi/main.go
+++ OrderApi/main.go
@@ -95,7 +95,7 @@ func Trace() gin.HandlerFunc {
 			Reporter: &jaegercfg.ReporterConfig{
 				//当span发送到服务器时要不要打日志
 				LogSpans:           true,
-				LocalAgentHostPort: "10.43.50.63:14250",
+				LocalAgentHostPort: "istio-system.jaeger-collector.svc.cluster.local:14250",
 			},
 			ServiceName: "gin",
 		}
