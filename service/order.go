aeb4c1671e58c0f58bf4de40b4e4ff363388cf07
likyam <384945266@qq.com>
2023-03-10 10:41:07 +0800

    配置链路追踪

--- service/order.go
+++ service/order.go
@@ -25,21 +25,22 @@ func (o orderService) mustEmbedUnimplementedOrderServiceServer() {
 	panic("implement me")
 }
 
-func (o orderService) getUserName(ctx context.Context) string {
+func (o orderService) getUserName(c context.Context) string {
 
 	// 解析metada中的信息并验证
-	md, ok := metadata.FromIncomingContext(ctx)
+	md, ok := metadata.FromIncomingContext(c)
 
 	if !ok {
 		return ""
 	}
 
+	fmt.Println(md["test-x-request-id"])
 	fmt.Println(md["x-request-id"])
-	//header := metadata.New(map[string]string{
-	//	"x-b3-traceid": md["x-request-id"][],
-	//})
-	//
-	//var ctx = metadata.NewOutgoingContext(context.Background(), header)
+	header := metadata.New(map[string]string{
+		"x-request-id": md["x-request-id"][0],
+	})
+
+	ctx := metadata.NewOutgoingContext(context.Background(), header)
 	conn, err := grpc.Dial("user-server:8003", grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
 		panic(err)
@@ -52,7 +53,7 @@ func (o orderService) getUserName(ctx context.Context) string {
 	}(conn)
 	userClient := NewUserClient(conn)
 	request := &UserRequest{Id: 1}
-	userInfo, err := userClient.GetUser(context.Background(), request)
+	userInfo, err := userClient.GetUser(ctx, request)
 	fmt.Println(userInfo)
 	return userInfo.GetUsername()
 }
